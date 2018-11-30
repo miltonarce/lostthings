1133c72e049018fe9d50d0d62b6025c12f08e637
mtorre4580 <mtorre4580@outlook.com>
2018-11-30 02:17:10 -0300

    merge fix

--- www/dist/js/bundle.js
+++ www/dist/js/bundle.js
@@ -953,18 +953,6 @@ angular.module("lostThings.services").factory("Items", [
   
 ]);
 
-angular.module("lostThings.services").factory("Profile", [
-  "$http",
-  "API_SERVER",
-  "Authentication",
-  function($http, API_SERVER, Authentication) {
-    //Header default para el token
-    const defaultHeader = {
-      headers: {
-        "X-Token": Authentication.getToken()
-      }
-    };
-
 angular
 .module('lostThings.services').factory('Profile', 
     ["$http",
@@ -1007,32 +995,19 @@ angular
             return $http.get(`${API_SERVER}/profile`, defaultHeader)
         }
 
-    /**
-     * Permite modificar la contraseña que posee el usuario
-     * @param {number} idUser
-     * @param {Object} requestPassword
-     */
-    function changePassword(idUser, requestPassword) {
-      return $http.put(
-        `${API_SERVER}/profile/${idUser}`,
-        userData,
-        defaultHeader
-      );
-    }
+        /**
+         * Permite obtener la información del usuario adicional
+         * @returns Promise
+         */
+        function getAdditionalInfo() {
+          return $http.get(`${API_SERVER}/profile`, defaultHeader);
+        }
 
-    /**
-     * Permite obtener la información del usuario adicional
-     * @returns Promise
-     */
-    function getAdditionalInfo() {
-      return $http.get(`${API_SERVER}/profile`, defaultHeader);
-    }
-
-    return {
-      edit: edit,
-      changePassword: changePassword,
-      getAdditionalInfo: getAdditionalInfo
-    };
+      return {
+        edit: edit,
+        changePassword: changePassword,
+        getAdditionalInfo: getAdditionalInfo
+      };
   }
 ]);
 
--- www/js/services/Profile.js
+++ www/js/services/Profile.js
@@ -1,15 +1,3 @@
-angular.module("lostThings.services").factory("Profile", [
-  "$http",
-  "API_SERVER",
-  "Authentication",
-  function($http, API_SERVER, Authentication) {
-    //Header default para el token
-    const defaultHeader = {
-      headers: {
-        "X-Token": Authentication.getToken()
-      }
-    };
-
 angular
 .module('lostThings.services').factory('Profile', 
     ["$http",
@@ -52,31 +40,18 @@ angular
             return $http.get(`${API_SERVER}/profile`, defaultHeader)
         }
 
-    /**
-     * Permite modificar la contraseña que posee el usuario
-     * @param {number} idUser
-     * @param {Object} requestPassword
-     */
-    function changePassword(idUser, requestPassword) {
-      return $http.put(
-        `${API_SERVER}/profile/${idUser}`,
-        userData,
-        defaultHeader
-      );
-    }
+        /**
+         * Permite obtener la información del usuario adicional
+         * @returns Promise
+         */
+        function getAdditionalInfo() {
+          return $http.get(`${API_SERVER}/profile`, defaultHeader);
+        }
 
-    /**
-     * Permite obtener la información del usuario adicional
-     * @returns Promise
-     */
-    function getAdditionalInfo() {
-      return $http.get(`${API_SERVER}/profile`, defaultHeader);
-    }
-
-    return {
-      edit: edit,
-      changePassword: changePassword,
-      getAdditionalInfo: getAdditionalInfo
-    };
+      return {
+        edit: edit,
+        changePassword: changePassword,
+        getAdditionalInfo: getAdditionalInfo
+      };
   }
 ]);
